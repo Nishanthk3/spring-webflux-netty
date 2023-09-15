57ff6d62c49e0fd032b3539f30abcfe3d4d05941
Nishanth Kommidi <nkommidi@yahooinc.com>
2023-09-14 23:16:43 -0700

    format code

--- src/main/java/com/webflux/demo/SpringWebFluxNettyApplication.java
+++ src/main/java/com/webflux/demo/SpringWebFluxNettyApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class SpringWebFluxNettyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWebFluxNettyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxNettyApplication.class, args);
+    }
 
 }
--- src/test/java/com/webflux/demo/SpringWebFluxNettyApplicationTests.java
+++ src/test/java/com/webflux/demo/SpringWebFluxNettyApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.MonoSink;
 
 @SpringBootTest
 class SpringWebFluxNettyApplicationTests {
@@ -23,15 +22,15 @@ class SpringWebFluxNettyApplicationTests {
         Mono<?> mono = Mono.just("Reactive programming").then(Mono.error(new RuntimeException("Exception Occurred")));
         mono.subscribe(System.out::println);
 
-		System.out.println("-------------------");
+        System.out.println("-------------------");
 
         mono = Mono.just("Reactive programming With log").then(Mono.error(new RuntimeException("Exception Occurred"))).log();
         mono.subscribe(System.out::println, throwable -> System.out.println(throwable));
     }
 
-	@Test
-	void testFlux() {
-		Flux<String> flux = Flux.just("One", "Two", "Three", "Five", "Four");
-		flux.subscribe(System.out::println);
-	}
+    @Test
+    void testFlux() {
+        Flux<String> flux = Flux.just("One", "Two", "Three", "Five", "Four");
+        flux.subscribe(System.out::println);
+    }
 }
